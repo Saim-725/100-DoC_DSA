107a2956a848e6704f4628f66219aeb09809f911
Saim-725 <msaimrke725@gmail.com>
2026-04-09 00:21:34 +0530

    41.c

---
+++ 41.c
@@ -0,0 +1,75 @@
+// Problem Statement:
+// Implement a Queue using a linked list supporting enqueue and dequeue operations.
+
+// Input Format:
+// - First line contains integer N
+// - Next N lines contain queue operations
+
+// Output Format:
+// - Print dequeued elements
+// - Print -1 if dequeue is attempted on an empty queue
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Front and Rear pointers
+struct Node *front = NULL, *rear = NULL;
+
+// Enqueue operation
+void enqueue(int x) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = x;
+    newNode->next = NULL;
+
+    if (rear == NULL) {
+        front = rear = newNode;
+        return;
+    }
+
+    rear->next = newNode;
+    rear = newNode;
+}
+
+// Dequeue operation
+void dequeue() {
+    if (front == NULL) {
+        printf("-1\n");
+        return;
+    }
+
+    struct Node* temp = front;
+    printf("%d\n", front->data);
+
+    front = front->next;
+
+    if (front == NULL)
+        rear = NULL;
+
+    free(temp);
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    while (N--) {
+        char op[10];
+        scanf("%s", op);
+
+        if (op[0] == 'e') {  // enqueue
+            int x;
+            scanf("%d", &x);
+            enqueue(x);
+        } else if (op[0] == 'd') {  // dequeue
+            dequeue();
+        }
+    }
+
+    return 0;
+}
