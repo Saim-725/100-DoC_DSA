3b4446bbf6cf609dc2b9ddae2401135cde311097
Saim-725 <msaimrke725@gmail.com>
2026-04-25 19:22:27 +0530

    49.c

---
+++ 49.c
@@ -0,0 +1,26 @@
+// Problem: BST Insert
+
+// Implement the solution for this problem.
+
+// Input:
+// - Input specifications
+
+// Output:
+// - Output specifications
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def insertIntoBST(root, val):
+    if root is None:
+        return TreeNode(val)
+    
+    if val < root.val:
+        root.left = insertIntoBST(root.left, val)
+    else:
+        root.right = insertIntoBST(root.right, val)
+    
+    return root
